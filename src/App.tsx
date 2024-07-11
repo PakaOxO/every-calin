c5d9f339a2852216d4658433de49566f9dd7c082
PakaOxO <arpeggiogio@gmail.com>
2024-07-11 17:56:22 +0900

    fix: ts-config emotion css props 옵션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
-import useCalendar from './hooks/useCalendar';
+import Calendar from './components/Calendar/Calendar';
 
 const App = () => {
-  const { getThisMonth } = useCalendar();
-  const today = new Date();
-  const prev = new Date(today.getFullYear(), today.getMonth() - 1, 1);
-
-  const dates = getThisMonth(prev, 6);
-  console.log(dates.join('\n'));
-
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
 };
 
 export default App;
