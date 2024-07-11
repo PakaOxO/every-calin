373bf0db07090f78962f9efa54d3501f2952c9df
PakaOxO <arpeggiogio@gmail.com>
2024-07-11 17:15:29 +0900

    Feature/#1: getThisMonth method add in useCalendar hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import './App.css';
+import useCalendar from './hooks/useCalendar';
 
 const App = () => {
+  const { getThisMonth } = useCalendar();
+  const today = new Date();
+  const prev = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+
+  const dates = getThisMonth(prev, 6);
+  console.log(dates.join('\n'));
+
   return <div className="App"></div>;
 };
 
